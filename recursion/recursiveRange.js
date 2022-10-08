e596a0dd5fb6c35e109a61466bee88b094b78578
MahmoudShaltoot <mahmoud.shaltoot@rubikal.com>
2022-10-08 05:06:48 +0200

    recursiveRange.js

---
+++ recursion/recursiveRange.js
@@ -0,0 +1,15 @@
+// Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function 
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55 
+
+function recursiveRange(number) {
+  if (number == 1) return 1;
+  return number + recursiveRange(number - 1)
+}
+
+(function main() {
+  console.log(recursiveRange(6));
+  console.log(recursiveRange(10));
+})();
