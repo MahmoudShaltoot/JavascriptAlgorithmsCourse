01adb1fa5d9d45c615fbc287cecd3533ddc651c0
MahmoudShaltoot <mahmoud.shaltoot@rubikal.com>
2022-12-23 22:15:06 +0200

    bubble sort optimization

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -1,19 +1,24 @@
-function bubble_sort (arr) {
-    for (let i = 1; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - i; j++) {
-            if (arr[j] > arr[j+1]) {
+function bubble_sort (arr, size) {
+    for (let steps = 0; steps < size - 1 ; steps++) {
+        let swap = 0;
+        for (let i = 0; i < size - steps - 1; i++) {
+            if (arr[i] > arr[i+1]) {
                 // swap
-                let tmp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = tmp
+                let tmp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = tmp
+
+                swap = 1;
             }
-        }        
+        }
+        // no swap means sorteed array
+        if (!swap) break;
     }
 }
 
 
 (function main() {
     const arr = [5,4,3,2,1];
-    bubble_sort(arr); 
+    bubble_sort(arr, arr.length); 
     console.log(arr);
 })();
